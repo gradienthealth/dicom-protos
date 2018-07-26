7d73d824416b80e5eeb430921712cda9ebed99b8
Suyash Kumar <suyashkumar2003@gmail.com>
2018-07-26 13:27:12 -0400

    Parse subattributes

--- parse/parse.go
+++ parse/parse.go
@@ -3,11 +3,14 @@ package parse
 import (
 	"encoding/json"
 	"io"
+	"log"
 	"strings"
 )
 
+// TagKey represents a DICOM Tag as a string "key" used in dicom-standard. Tag (aaaa, bbbb) => aaaabbbb as a key
 type TagKey string
 
+// Attribute represents a DICOM attribute, with ability to point to "sub-attributes" that may be in a SQ
 type Attribute struct {
 	Tag                 string `json:"tag"`
 	Name                string `json:"name"`
@@ -19,12 +22,14 @@ type Attribute struct {
 	SubAttributes       map[TagKey]*Attribute
 }
 
+// IsEmpty returns true if the Attribute is effectively empty
 func (a *Attribute) IsEmpty() bool {
 	return a.Name == "" && a.Keyword == "" &&
 		a.ValueRepresentation == "" &&
 		a.ValueMultiplicity == "" && a.Tag == ""
 }
 
+// Returns the corresponding TagKey of an Attribute
 func (a *Attribute) TagKey() TagKey {
 	if a.tagKey != "" {
 		return a.tagKey
@@ -33,21 +38,36 @@ func (a *Attribute) TagKey() TagKey {
 	return a.tagKey
 }
 
-func (a *Attribute) AddSubAttribute(at *Attribute, keys []TagKey) {
+// AddSubAttribute adds a subattribute in the right place to an attribute "tree"
+func (a *Attribute) AddSubAttribute(at *Attribute, keys []TagKey, attributeMap AttributeMap) {
 	if len(keys) == 1 {
-		a.SubAttributes[keys[0]] = at
+		if a.SubAttributes == nil {
+			a.SubAttributes = make(map[TagKey]*Attribute)
+		}
+		attr := attributeMap[keys[0]]
+		a.SubAttributes[keys[0]] = &attr //TODO: don't use addresses, copy prob happens anyway
+		return
 	}
 
-	// If map hasn't been made yet, do so
+	// If a's subattribute map hasn't been made yet, do so
 	if a.SubAttributes == nil {
 		a.SubAttributes = make(map[TagKey]*Attribute)
 	}
 
-	a.SubAttributes[keys[1]].AddSubAttribute(at, keys[1:])
+	/*
+		// Add current level attribute to a's subattributes. DONT HAVE TO DO THIS assuming ordering in JSON
+		attr := attributeMap[keys[0]]
+		a.SubAttributes[keys[0]] = &attr //TODO: dont use addresses, copy prob happens anyway
+	*/
+
+	// Call add sub attribute on the next level
+	a.SubAttributes[keys[0]].AddSubAttribute(at, keys[1:], attributeMap)
 }
 
+// AttributeMap represents a mapping from TagKey to Attribute entities
 type AttributeMap map[TagKey]Attribute
 
+// Module represents a DICOM module
 type Module struct {
 	ID             string `json:"id"`
 	Name           string `json:"name"`
@@ -56,6 +76,7 @@ type Module struct {
 	Attributes     map[TagKey]*Attribute
 }
 
+// AddAttribute adds a top level attribute to a module
 func (m *Module) AddAttribute(a Attribute, path string) {
 	if m.Attributes == nil {
 		m.Attributes = make(map[TagKey]*Attribute)
@@ -84,9 +105,21 @@ type ModuleToAttributeItem struct {
 	Path     string `json:"path"`
 }
 
-func Parse(attributeFile, moduleFile, moduleToAttributesFile io.Reader) ([]*Module, error) {
+// PathTagKeys returns a slice of TagKeys from the path of the ModuleToAttributeItem
+func (m ModuleToAttributeItem) PathTagKeys() []TagKey {
+	keys := strings.Split(m.Path, ":")[1:]
+	tagKeys := make([]TagKey, len(keys))
+	// Cast strings to TagKeys
+	for i := range keys {
+		tagKeys[i] = TagKey(keys[i])
+	}
+	return tagKeys
+}
+
+// Parse consumes data from innolitics' dicom-standard format and returns a slice of parsed Modules.
+func Parse(attributeSource, moduleSource, moduleToAttributesSource io.Reader) ([]*Module, error) {
 	// Parse AttributeMap
-	dec := json.NewDecoder(attributeFile)
+	dec := json.NewDecoder(attributeSource)
 	var attrMap AttributeMap
 	err := dec.Decode(&attrMap)
 	if err != nil {
@@ -94,7 +127,7 @@ func Parse(attributeFile, moduleFile, moduleToAttributesFile io.Reader) ([]*Modu
 	}
 
 	// Parse ModuleMap
-	dec = json.NewDecoder(moduleFile)
+	dec = json.NewDecoder(moduleSource)
 	var mMap ModuleMap
 	err = dec.Decode(&mMap)
 	if err != nil {
@@ -103,7 +136,7 @@ func Parse(attributeFile, moduleFile, moduleToAttributesFile io.Reader) ([]*Modu
 	moduleMap := mMap.ToPointerMap()
 
 	// Parse ModuleToAttributeItem mappings
-	dec = json.NewDecoder(moduleToAttributesFile)
+	dec = json.NewDecoder(moduleToAttributesSource)
 	var moduleToAttributes []ModuleToAttributeItem
 	err = dec.Decode(&moduleToAttributes)
 	if err != nil {
@@ -113,8 +146,26 @@ func Parse(attributeFile, moduleFile, moduleToAttributesFile io.Reader) ([]*Modu
 	// Associate all modules with their sets of attributes
 	for _, mapping := range moduleToAttributes {
 		tagKey := tagToKey(mapping.Tag)
+		attr, ok := attrMap[tagKey]
+		if !ok {
+			log.Printf("ERROR: NO Attribute MAPPING FOUND: %s", tagKey)
+		}
 		module := moduleMap[mapping.ModuleID]
-		module.AddAttribute(attrMap[tagKey], mapping.Path)
+
+		// Add the attribute to the module or corresponding module attribute chain:
+		pathTagKeys := mapping.PathTagKeys()
+
+		if len(pathTagKeys) == 1 {
+			// Add as a top level module attribute
+			module.AddAttribute(attr, mapping.Path)
+		} else {
+			// This attribute is nested under attributes, add as a sub attribute for the proper attribute
+			if module.Attributes == nil {
+				module.Attributes = make(map[TagKey]*Attribute)
+			}
+
+			module.Attributes[pathTagKeys[0]].AddSubAttribute(&attr, pathTagKeys[1:], attrMap)
+		}
 	}
 
 	// Create slice of modules to return
@@ -133,6 +184,7 @@ func tagToKey(tag string) TagKey {
 	s := strings.Replace(tag, ",", "", -1)
 	s2 := strings.Replace(s, "(", "", -1)
 	s3 := strings.Replace(s2, ")", "", -1)
+	s4 := strings.ToLower(s3)
 
-	return TagKey(s3)
+	return TagKey(s4)
 }
